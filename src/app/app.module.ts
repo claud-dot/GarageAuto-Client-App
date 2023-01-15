7682ea0545f6125c7dc3228e985de1baea98bb7f
claudMJA <81508316+claud-dot@users.noreply.github.com>
2023-01-15 18:07:26 +0300

    create folder app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { AuthComponent } from './components/auth/auth.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { RepairComponent } from './components/home/repair/repair.component';
+import { CarComponent } from './components/home/car/car.component';
+import { HistoryComponent } from './components/home/history/history.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AuthComponent,
-    WelcomeComponent
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent,
+    FooterComponent,
+    RepairComponent,
+    CarComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
