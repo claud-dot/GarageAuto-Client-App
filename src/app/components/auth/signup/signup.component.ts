18aafe84ec0af59d601829350556ec0ddfe37f78
claudMJA <81508316+claud-dot@users.noreply.github.com>
2023-01-16 23:09:21 +0300

    signup and add environement folder

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -17,13 +17,9 @@ export class SignupComponent {
 
   initForm(){
     this.signUpForm = this.formbuild.group({
-        name : new FormControl(null , Validators.compose([
+        username : new FormControl(null , Validators.compose([
           Validators.required,
           Validators.minLength(4)
-        ])),
-        lastname : new FormControl(null , Validators.compose([
-           Validators.required , 
-           Validators.minLength(4)
         ])),
         email : new FormControl(null , Validators.compose([
           Validators.required,
@@ -35,21 +31,15 @@ export class SignupComponent {
             Validators.required,
             Validators.pattern(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W)/),
             Validators.minLength(6)
-        ])),
-        confirm : new FormControl(null,Validators.required)
+        ]))
     })
   }
 
-  onchangeClass(formName : string){
-      if(this.signUpForm.get(formName)?.valid){
-          return 'form-control is-valid';
-      }else if(this.signUpForm.get(formName)?.errors && (this.signUpForm.get(formName)?.dirty || this.signUpForm.get(formName)?.touched)){
-          //this.errorStyle = true;
-          return 'form-control is-invalid';
-      }else{
-          return 'form-control';
-      }            
-  }
+  onBlurForm(formName : string , elementId : string) {}
+
+  onSubmitForm(){}
+
+  onHidePass( hide : boolean ){}
 
   initHide(){
       this.hide.pass = true;
