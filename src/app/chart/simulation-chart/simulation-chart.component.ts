8821b42d55beb9f255c720ab006554669f8953c5
claudMJA <81508316+claud-dot@users.noreply.github.com>
2023-01-28 17:37:22 +0300

    statistique update

--- src/app/chart/simulation-chart/simulation-chart.component.ts
+++ src/app/chart/simulation-chart/simulation-chart.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+
+import annotationPlugin from 'chartjs-plugin-annotation';
 
 @Component({
   selector: 'app-simulation-chart',
   templateUrl: './simulation-chart.component.html',
   styleUrls: ['./simulation-chart.component.css']
 })
-export class SimulationChartComponent {
+export class SimulationChartComponent implements OnInit {
+
+  @Input() dataChart : any = {};
+  
+  labels : string[];
+  type : string;
+  data : any[];
+
+  labelsChart = [
+    { ref : "jour" , label : ['Lundi' , 'Mardi' , 'Mercredi' ,'Jeudi' , 'Vendredi' , 'Samedi', 'Dimanche'] },
+    { ref : "mois" , label : ['Janvier','Fevrier' , 'Mars' , 'Avril' , 'Mais' ,'Juin' ,'Juillet' , 'Août' ,'Septembre' , 'Novembre' ,'Decembre']}
+  ];
+
+  constructor() {
+    Chart.register(...registerables);
+    Chart.register(annotationPlugin);
+  }
+
+  initDataChart(){
+    this.labels = this.dataChart.data.labels
+    this.type = this.dataChart.type;
+    this.data = this.dataChart.data.data;
+  }
+
+  simulationChart(){
+    this.initDataChart();
+    const labels = this.labels;
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Bénefice par chiffre d\'affaire',
+          data: this.data,
+          borderColor: "transparent",
+          backgroundColor:"#ed143dcc",
+          borderWidth: 1
+        }
+      ]
+    }
+
+    const config : any= {
+      type: 'bar',
+      data: data,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top' as const,
+            display: false,
+          },
+          title: {
+            display: false,
+            text: 'Chart.js Line Chart',
+          },
+        }
+      },
+    };
+
+    new Chart('simule-chart' , config);
+  }
+
+  ngOnInit(): void {
+    this.simulationChart();
+  }
+
 
 }
--- src/app/components/home/financier/statistic/statistic.component.ts
+++ src/app/components/home/financier/statistic/statistic.component.ts
@@ -1,10 +1,12 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { debounceTime, Subscription } from 'rxjs';
 import { CarService } from 'src/app/services/car.service';
-import { StorageService } from 'src/app/services/storage.service';
+import { UserService } from 'src/app/services/user.service';
 import { UtlisService } from 'src/app/services/utlis.service';
+
+declare var $ : any;
 
 @Component({
   selector: 'app-statistic',
@@ -15,23 +17,39 @@ export class StatisticComponent implements OnInit {
 
   formCtrlSub : Subscription;
   barFormSub : Subscription;
+  simuleFormSub : Subscription;
+  addBtn : string = "Global";
+
   modelMark : string[];
+  listSimule : any = [];
   loading : any = {};
+
   lineForm : FormGroup;
   barForm : FormGroup;
+  simuleForm : FormGroup;
+  simuleTypeForm : FormGroup;
+
+  months : string[] = ['Janvier' , 'Février' ,'Mars' , 'Avril' , 'Mai' ,'Juin' ,'Juillet' , 'Aôut' , 'Septembre' , 'Octobre' , 'Novembre' , 'Décembre'];
+  monthtsFilter: string[];
+  inputSimule :any[] = [
+    {name : "Salaire" , value : 'salary' , place : "salaire"},
+    {name : "Loyer" , value : 'rent' , place : "loyer"},
+    {name : "Piece" , value : 'piece' , place : "achats des pèice"},
+    {name : "Autres" , value : 'other' , place : "autres dépenses"},
+  ]
+
   dataLineChart : any = { mark : "" , model : "" };
   dataBarChart : string = "null" ;
-  dataLineStat : any = {
-      label : "Nombre de jour",
-      type : "line", 
-  };
-  dataBarStat : any = {
-    data : [] ,
-    label : "Nombre de jour",
-    type : "line", 
+  dataSimulChart : any = {
+    data : { salary : 0 , rent : 0 ,piece : 0 ,other : 0, mont : "" },
+    choice : "global"
   };
 
-  constructor(private carService : CarService , private utils : UtlisService , private build:FormBuilder) {}
+  dataSimulStat : any = { label : "Montant simuler en Ar", type : "bar" }
+  dataLineStat : any = { label : "Nombre de jour", type : "line", };
+  dataBarStat : any = {  label : "Montant en Ar", type : "bar" };
+
+  constructor(private carService : CarService , public userService: UserService , private utils : UtlisService , private build:FormBuilder) {}
 
   initLineForm(){
     this.lineForm = this.build.group({
@@ -43,6 +61,22 @@ export class StatisticComponent implements OnInit {
   initBarForm(){
     this.barForm = this.build.group({
         unit_duration : new FormControl("") 
+    })
+  }
+
+  initSimuleForm(){
+    this.simuleForm = this.build.group({
+      salary : new FormControl(null , [ Validators.min(0), Validators.required]),
+      rent : new FormControl(null , [Validators.min(0), Validators.required]),
+      piece : new FormControl(null , [Validators.min(0), Validators.required]),
+      other : new FormControl(null , [Validators.min(0), Validators.required]),
+      month : new FormControl("")
+    });
+  }
+
+  initSimuleTypeForm(){
+    this.simuleTypeForm = this.build.group({
+      choice : new FormControl('')
     })
   }
 
@@ -82,8 +116,6 @@ export class StatisticComponent implements OnInit {
   getTurnover(){
     this.loading.stat_turnover = true;
     const success = (data : any)=>{
-      console.log(data.dataStat);
-      
       this.dataBarStat.data = data.dataStat;
       this.loading.stat_turnover = false;
     }
@@ -97,10 +129,28 @@ export class StatisticComponent implements OnInit {
     this.carService.getTurnoverStat(this.dataBarChart).subscribe(success, error);
   }
 
+  getBenefice(){
+    this.loading.simulation = true;
+    const success = (data : any)=>{
+      this.dataSimulStat.data = data.dataStat;
+      console.log(data);
+      
+      this.loading.simulation = false;
+      $('.collapse').collapse('hide');
+      this.listSimule =[];
+    }
+
+    const error = (error : HttpErrorResponse)=>{
+      this.utils.openToastr( error.error , " Simulation Statistique" ,"error");
+      console.log("Simulation stat : "+error.error);
+      console.log(" Simulation status : "+error.status);
+      this.loading.simulation = false;
+    }
+    this.userService.getSimulationStat(this.dataSimulChart).subscribe(success, error);
+  }
+
   ngAfterContentInit() {
-    this.formCtrlSub = this.lineForm.valueChanges
-    .pipe(debounceTime(500))
-    .subscribe(value => {
+    this.formCtrlSub = this.lineForm.valueChanges.pipe(debounceTime(500)).subscribe(value => {
           if(value.mark.trim()!=""){
             this.modelMark = this.dataLineStat.cars.find((car : any)=>car.mark ==value.mark).models;
             const indexModelValue = this.modelMark.findIndex((model : any)=>model ==value.model);
@@ -110,19 +160,76 @@ export class StatisticComponent implements OnInit {
           this.getRepairStat();
       });
 
-    this.barFormSub = this.barForm.valueChanges
-    .pipe(debounceTime(500))
-    .subscribe(value =>{
+    this.barFormSub = this.barForm.valueChanges.pipe(debounceTime(500)).subscribe(value =>{
         this.dataBarChart = value.unit_duration; 
         this.getTurnover();
-    })
+    });
+
+    this.simuleFormSub = this.simuleTypeForm.valueChanges.pipe(debounceTime(200)).subscribe(value =>{
+        $('.collapse').collapse('hide');
+        this.addBtn = value.choice;
+        this.simuleForm.reset();
+        this.listSimule =[];
+        if(this.addBtn=='Mois') {
+          this.simuleForm.reset();
+          this.formReset();
+        }
+    });
+  }
+
+  onSimule(){
+    if(this.simuleTypeForm.value['choice']!="Mois"){
+      this.dataSimulChart.data= this.simuleForm.value;
+      this.dataSimulChart.choice = "global";
+      this
+    }else{
+      this.dataSimulChart.data = this.listSimule;
+      this.dataSimulChart.choice = "mois";
+      $('.collapse').collapse('hide');
+      this.listSimule =[];
+    }
+    this.getBenefice();
+    console.log(this.dataSimulChart);
+    this.formReset();
+  }
+
+  onAddSimule(){
+    $('.collapse').collapse('show');
+    this.listSimule.push(this.simuleForm.value);
+    this.formReset();
+  }
+
+  formReset(){
+    this.simuleForm.reset();
+    this.monthtsFilter = this.months;
+    let listMonthSimule : string[]= [];
+    for (let simul of this.listSimule) {
+      listMonthSimule.push(simul.month);
+    }
+    this.monthtsFilter =this.monthtsFilter.filter((val : string) => !listMonthSimule.includes(val));
+    this.simuleForm.controls['month'].setValue(this.monthtsFilter[0]);
+  }
+
+  onDeleteSimule(index: number){
+    this.listSimule.splice(index , 1);
+    if(this.listSimule.length<=0){
+      $('.collapse').collapse('hide');
+    }
+    this.formReset();
+  }
+
+  scrollTo(section : string){
+    document.querySelector("#"+section)?.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
   }
 
   ngOnInit(): void {
+    this.initSimuleTypeForm();
     this.initLineForm();
     this.initBarForm();
+    this.initSimuleForm();
     this.getRepairStat();
     this.getCars();
     this.getTurnover();
+    this.getBenefice();
   }
 }
